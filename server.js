6f94b1a4f7e97e2a154c571f6bd96e43bbba288b
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-07-01 22:41:22 +0530

    Added MongoDB connection to server.js

--- server.js
+++ server.js
@@ -12,3 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.static(path.join(__dirname)));
+
+const mongoURL = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/students';
+mongoose.connect(mongoURL, {
+  useNewUrlParser: true,
