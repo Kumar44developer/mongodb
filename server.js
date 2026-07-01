67175bbed36d39cf749dd592b76c4014f8a9d2fd
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-07-01 22:40:33 +0530

    Imported CORS, path, and dotenv modules
    Added CORS and path modules, and loaded environment variables.

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+require('dotenv').config();
